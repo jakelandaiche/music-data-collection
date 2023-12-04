08891fa7b008fe2180ba3e0b66eff3061e9da724
jdl352 <109162620+jdl352@users.noreply.github.com>
2023-12-04 09:07:13 -0500

    commit changes so they save correctly

--- game.py
+++ game.py
@@ -5,8 +5,10 @@ import random
 from data import get_videos
 from database import Database
 
+
 def decide_winner(answers):
     return random.choice(answers)
+
 
 class Game:
     def __init__(self, room, N):
@@ -18,11 +20,13 @@ class Game:
 
     async def run(self):
         # game explanation
-        self.room.room_text = f"<h3>MusicBox</h3>" + "Welcome to MusicBox. The game will start soon"
+        self.room.room_text = (
+            f"<h3>MusicBox</h3>" + "Welcome to MusicBox. The game will start soon"
+        )
         await self.room.update_frontend()
         await self.room.set_countdown(15)
         await asyncio.sleep(17)
-        
+
         for player in self.room.players.values():
             player.db_id = self.db.create_player(player.name, self.game_id)
 
@@ -30,12 +34,18 @@ class Game:
             # play audio
             for player in self.room.players.values():
                 player.answer = ""
-            self.room.show_answers = False 
+            self.room.show_answers = False
             video = get_videos()[0]
-            msg = {"type": "video", "id": video["id"], "start_time": video["start_time"]}
+            msg = {
+                "type": "video",
+                "id": video["id"],
+                "start_time": video["start_time"],
+            }
             await self.room.websocket.send(json.dumps(msg))
 
-            self.room.room_text = f"<h3>Round {i+1}</h3>" + "Listen to this audio carefully"
+            self.room.room_text = (
+                f"<h3>Round {i+1}</h3>" + "Listen to this audio carefully"
+            )
             await self.room.update_frontend()
             await self.room.set_countdown(15)
             await asyncio.sleep(17)
@@ -50,7 +60,7 @@ class Game:
             answers = []
             for player in self.room.players.values():
                 answers.append(player.answer)
-                self.db.write_answer(player, i) # add score here
+                self.db.write_answer(player, i)  # add score here
 
             print(f"results for {video['id']}")
             print(answers)
@@ -60,7 +70,7 @@ class Game:
             await self.room.set_countdown(15)
             await asyncio.sleep(17)
 
-
-
         self.room.room_text = "The game is over. Thanks for playing"
+        self.db.cur.close()
+        self.db.con.close()
         await self.room.update_frontend()
