41786d5e933eded95db4387467a983b1bd76797b
Jasper Ty <jasperty2@gmail.com>
2023-12-06 19:03:53 -0500

    update gitignore and add filter script

---
+++ filter_unplayable.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+from yt_dlp import YoutubeDL
+from alive_progress import alive_bar
+
+_dataset = pd.read_csv(
+  "eval_segments.csv",
+  sep=", ",
+  on_bad_lines="skip",
+  skiprows=2,
+  quotechar='"',
+  engine="python"
+)
+
+dataset = _dataset[_dataset["positive_labels"].str.match(".*/m/04rlf.*")]
+
+playable = []
+with alive_bar(int(dataset.size)) as bar:
+  with YoutubeDL({ "quiet": True }) as ydl: 
+    for video_id in dataset['# YTID']:
+      URL = f'https://www.youtube.com/watch?v={video_id}'
+      try:
+        info = ydl.extract_info(video_id, download=False)
+        playable.append(info.get('playable_in_embed'))
+      except:
+        playable.append(False)
+      bar()
+      
+filtered = dataset[playable]
+filtered.to_csv('filtered.csv')
